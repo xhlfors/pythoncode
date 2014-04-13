efd2fde56cc4734a9c8952fef98e5bf2b0a307fa
zaga <ys.zhang.thu@gmail.com>
2014-04-13 10:27:35 +0800

    add some methods to pl
    propose that the player act through out class act functions
    player.action(function)
    
    correct some errors

--- Base.py
+++ Base.py
@@ -2,21 +2,24 @@
 from time import ctime
 class Game(object):
     def __init__(self,pnum,gname=''):
-        state=''
-        player_num=pnum
-        name=gname  #game name
-        talk_log=[]# talk_log=[(id,talking)]
+        self.state=''
+        self.player_num=pnum
+        self.name=gname  #game name
+        self.talk_log=[]# talk_log=[(id,talking)]
     pass
+
+
+
 class Player(object):
     "the player may can 'speak'"
     def __init__(self,idnum,st=False):
-        cards=[]    #card at hand
-        card_hist=[]
-        can_act=False #if the player  can do action
-        saying=(-1,'')   #present talkind words. say '' at time '-1'
-        said=[]    #talking history
-        state=st # indicating if the player is in the game
-        id=idnum
+        self.cards=[]    #card at hand
+        self.card_hist=[]
+        self.can_act=False #if the player  can do action
+        self.saying=(-1,'')   #present talkind words. say '' at time '-1'
+        self.said=[]    #talking history
+        self.state=st # indicating if the player is in the game
+        self.id=idnum
     
     def add_card(self, card):
         self.cards.append(card)
@@ -24,14 +27,13 @@ class Player(object):
         "delete card from cards and add it to card_hist, if card not specified del the last one. if succeed return 0 else -1"
         if card==None:
             self.cards_hist.append(self.cards.pop())
-            return 0
-        elif:
+        else:
             try:
                 i=self.cards.index(card)
             except ValueError:
                 return -1
             self.cards_hist.append(self.cards.pop(i))
-            return 0
+        return 0
 
     def get_id(self):
         return self.id
@@ -39,13 +41,15 @@ class Player(object):
         return [cd.name for cd in self.cards]
     def get_cards_histname(self):
         return [cd.name for cd in self.cards_hist]
-    
 
     def action(self,act):
         "the player can do some actions for example select cards"
-        #act is a function let player to do something
-        pass
+        #act is a function let player to do something suc return 0 else -1
+        return act(self)
     pass
+
+
+
 class Card(object):
     "the card should can take information and send information"
     actions=[] #the card can do something
@@ -67,3 +71,7 @@ def open_game(*arg,**argkw):
 
 if __name__=="__main__":
     print 'biuld succeed'
+    p1=Player(idnum=12)
+    g1=Game(32)
+    g1.player_num
+    print p1.id
--- ChangeHist.md
+++ ChangeHist.md
@@ -1,2 +1,7 @@
 pythoncode
 ==========
+4.12
+-------
+        add some methods to player class
+        propose that the player act through out class act functions
+            player.action(function)
